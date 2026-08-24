4427b5d18f611ffb67396cd66bafbd2a9d6565f4
TWJohnJohn20116 <patch.py.5487@gmail.com>
2026-08-24 12:10:43 +0800

    fix: replace fragile reflection with direct maps for Action and ActionProcessor mappings

--- api/src/main/kotlin/com/coderxi/plugin/fakeplayer/api/action/Action.kt
+++ api/src/main/kotlin/com/coderxi/plugin/fakeplayer/api/action/Action.kt
@@ -9,19 +9,17 @@ sealed interface Action {
     val supportModes : Collection<Class<out ActionMode>>
     val mode: ActionMode
     companion object {
-        private val mapping = listOf(
-            AttackAction::class.java,
-            MineAction::class.java,
-            UseItemAction::class.java,
-            DropItemAction::class.java,
-            JumpAction::class.java,
-            SneakAction::class.java,
-            FlattenAction::class.java,
-        ).associateBy {
-            it.getDeclaredField("type").get(null) as ActionType
-        }
+        private val mapping: Map<ActionType, Class<out Action>> = mapOf(
+            ActionType.ATTACK to AttackAction::class.java,
+            ActionType.MINE to MineAction::class.java,
+            ActionType.USE_ITEM to UseItemAction::class.java,
+            ActionType.DROP_ITEM to DropItemAction::class.java,
+            ActionType.JUMP to JumpAction::class.java,
+            ActionType.SNEAK to SneakAction::class.java,
+            ActionType.FLATTEN to FlattenAction::class.java,
+        )
         private val supportModesCache = ConcurrentHashMap<ActionType, List<Class<out ActionMode>>>()
-        fun toClass(type: ActionType) = mapping[type]!!
+        fun toClass(type: ActionType): Class<out Action> = mapping[type]!!
         fun getSupportModes(type: ActionType) = supportModesCache.computeIfAbsent(type) { type ->
             mapping[type]?.declaredConstructors
                 ?.mapNotNull { it.parameterTypes.firstOrNull() }
--- plugin/src/main/kotlin/com/coderxi/plugin/fakeplayer/action/ActionProcessorRegistry.kt
+++ plugin/src/main/kotlin/com/coderxi/plugin/fakeplayer/action/ActionProcessorRegistry.kt
@@ -6,17 +6,15 @@ import com.coderxi.plugin.fakeplayer.api.action.ActionType
 
 object ActionProcessorRegistry {
 
-    private val processors = listOf(
-        AttackProcessor,
-        MineProcessor,
-        UseItemProcessor,
-        DropItemProcessor,
-        JumpProcessor,
-        SneakProcessor,
-        FlattenProcessor
+    private val registry: Map<ActionType, ActionProcessor<*>> = mapOf(
+        ActionType.ATTACK to AttackProcessor,
+        ActionType.MINE to MineProcessor,
+        ActionType.USE_ITEM to UseItemProcessor,
+        ActionType.DROP_ITEM to DropItemProcessor,
+        ActionType.JUMP to JumpProcessor,
+        ActionType.SNEAK to SneakProcessor,
+        ActionType.FLATTEN to FlattenProcessor
     )
-
-    private val registry = processors.associateBy { it.actionType.getDeclaredField("type").apply { isAccessible = true }.get(null) as ActionType }
 
     @Suppress("UNCHECKED_CAST")
     fun <T : Action> get(action: T): ActionProcessor<T>? {
